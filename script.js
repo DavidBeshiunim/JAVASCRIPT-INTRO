6ec046a820862792e82535ba45596ff669cc2713
Davidbeshiunim <santusandor@gmail.com>
2024-12-16 18:19:02 +0000

    Javascript intro

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("myheading").innerHTML = "Heading from Javascript";//statement 1//
+
+document.getElementById("myparagraph").innerHTML = "Heading from Javascript";//statement 2//
+
+// Variables: A box box that holds/stores value
+var num1 = 10;
+var num2 = 8;
+var add_result = num1 + num2;
+document.getElementById("add").innerHTML = add_result;
+
+var num3 = 50;
+var num4 = 45;
+document.getElementById("subtract").innerHTML = num3 - num4;
+
+var a = 5;
+a += 2;//Addition assignment//
+// a = a +2;
+document.write(a);
